74dacb097660c99cd916584595f17cbb95867120
HeitorMesquita <94998317+HeitorMesquita@users.noreply.github.com>
2022-02-15 22:06:40 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ var seaImg,shipImg;
 function preload(){
   // Descomente o código para adicionar animação ao navio
 
-  shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1.png");
   //shipImg1 = loadAnimation("ship-1.png");
   //shipImg1 = loadAnimation("ship-1");
-  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
   //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
   
   seaImg = loadImage("sea.png");
